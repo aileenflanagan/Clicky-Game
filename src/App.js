dec2be275455fe259500fa24f80d6c4330153154
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-03-25 12:09:45 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,71 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">The Clicky Game</h1>
+          <p class="lead">Click on an image, but don't click an image more than once!</p>         
+          <h1><span class="badge badge-primary">Score: 0</span><span class="badge badge-success"> High Score: 0</span></h1>          
+        </div>
       </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+        </div>
+        <br></br>
+        <div class="row">
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+        </div>
+        <br></br>
+        <div class="row">
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-2">
+          <img src="" alt="..." class="img-thumbnail"></img>
+          </div>
+        </div>
+      </div>
+      </>
     );
   }
 }
