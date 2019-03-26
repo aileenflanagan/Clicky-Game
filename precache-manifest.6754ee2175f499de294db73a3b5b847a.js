fa11025b09e5d84560e9988884c3b61fdda0ae79
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-03-25 23:51:15 -0500

    Updates

---
+++ precache-manifest.6754ee2175f499de294db73a3b5b847a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.895984e4.js"
+  },
+  {
+    "revision": "143094484359eef35a1f",
+    "url": "/clicky-game/static/js/main.9c5e3785.chunk.js"
+  },
+  {
+    "revision": "2623bd57ac06f6bb6f83",
+    "url": "/clicky-game/static/js/2.6a00199c.chunk.js"
+  },
+  {
+    "revision": "143094484359eef35a1f",
+    "url": "/clicky-game/static/css/main.0f1da799.chunk.css"
+  },
+  {
+    "revision": "b9f7b22cee0b049f208999772c187335",
+    "url": "/clicky-game/index.html"
+  }
+];
